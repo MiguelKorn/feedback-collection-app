cb3d4af10b6b3a9dbf891572cf3d204af731f45a
Miguel Korn <20448@ma-web.nl>
2017-10-20 17:08:19 +0200

    added mongoDB

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
 
-const PORT = process.env.PORT || 3000;
+// Add Models
+require('./models/UserModel');
 
 // Add Services
 require('./services/passport');
 
+mongoose.connect(keys.mongoURI);
+
+const app = express();
+
+app.use(cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Add Routes
-require('./routes/auth')(app);
+require('./routes/authRoutes')(app);
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT);
